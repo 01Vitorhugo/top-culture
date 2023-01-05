b8d47c244b7f5047b4bc03e9850b966598859ad7
Vitor Hugo <vitorhugo0031@outlook.com>
2023-01-05 19:07:37 -0300

    tela e acao de cadastro criadas

--- cadastro.php
+++ cadastro.php
@@ -10,9 +10,25 @@
 <body>
     <div class="cad">
         <h1>Cadastro de Cliente</h1>
+        <form action="cadastro.act.php" method="post" enctype="multipart/form-data">
         <div class="content">
             <p>Nome</p>
-            <input type="text" name="" id="">
+            <input type="text" name="nome" id="">
+
+            <p>Sobrenome</p>
+            <input type="text" name="sobrenome" id="">
+
+            <p>E-mail</p>
+            <input type="text" name="email" id="">
+
+            <p>Senha</p>
+            <input type="password" name="senha" id="">
+
+            <p><input type="submit" value="Criar-Conta"></p>
+
+            </form>
+
+            
         </div>
 
     </div>
