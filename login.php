485f75ecefd6dc826f4f6437ec4026eaf5740145
Vitor Hugo <vitorhugo0031@outlook.com>
2023-01-06 19:27:12 -0300

    ajustes de login

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/login.css">
+    <title>Login</title>
+</head>
+<body>
+
+<form action="login.act.php" method="post">
+
+<div class="msg-php">
+
+<?php
+@session_start();
+if(isset($_SESSION['msg'])){
+    echo "<p class=alert-php>$_SESSION[msg]</p>";
+    unset($_SESSION['msg']);
+}
+?>
+
+<input type="text" name="email" placeholder="E-mail">
+            <input type="password"  name="senha" placeholder="Senha:">
+            <input id="dif" type="submit" value="CONTINUAR">
+
+            <h4>Ainda não tem cadastro? <a href="cadastro.php"> cadastre-se</a></h4>
+
+
+
+</div>
+
+
+    
+</form>
+    
+</body>
+</html>
