c5ce52f09b85f073975e0fff4823bc43da782b4d
PIGSx <hiury1315ajac@gmail.com>
2023-01-10 21:31:33 -0300

    arrumando bug no scroll

--- js/scroll.js
+++ js/scroll.js
@@ -4,7 +4,6 @@ const sr = ScrollReveal ({
     reset : false,
   })
 
-sr.reveal('.news',{ delay:100})
 sr.reveal('.slider',{ delay:250, origin:'left' })
 sr.reveal('.services',{ delay:350, origin:'right'})
 sr.reveal('.content',{ delay:400, origin:'left' })
