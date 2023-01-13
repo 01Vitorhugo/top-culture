7c822808d5f252d125c36f57911fad08e80dce58
PIGSx <hiury1315ajac@gmail.com>
2023-01-12 23:43:17 -0300

    exclui sem querer

--- js/scroll.js
+++ js/scroll.js
@@ -6,6 +6,6 @@ const sr = ScrollReveal ({
 
 sr.reveal('.slider',{ delay:250, origin:'left' })
 sr.reveal('.services',{ delay:350, origin:'right'})
-sr.reveal('.content',{ delay:400, origin:'left' })
+sr.reveal('.info',{ delay:400, origin:'left' })
 sr.reveal('.producer',{ delay:350, origin:'right' })
 sr.reveal('.convite',{ delay:300, origin:'left' })
